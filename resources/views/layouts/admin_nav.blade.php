cf4dc9e35cdb9b6425d700b0e1f9cefe917789ce
Presian Grigorov <p.grigorov@ardes.bg>
2022-08-22 11:34:15 +0300

    Added an 'Exit Admin Panel' button

--- resources/views/layouts/admin_nav.blade.php
+++ resources/views/layouts/admin_nav.blade.php
@@ -39,6 +39,9 @@
                         @guest
                             
                         @else
+                        <a class="nav-link" href="{{ route('home') }}">
+                           Exit Admin Panel
+                        </a>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
